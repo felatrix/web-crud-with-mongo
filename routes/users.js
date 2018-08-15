6164102eb3f1aa1e59f85b1475ba495381200b93
Iwan Darmawan <iwansaila@gmail.com>
2018-08-15 14:45:44 +0700

    add register validation

--- routes/users.js
+++ routes/users.js
@@ -13,4 +13,33 @@ router.get('/register',(req,res)=>{
     res.render('users/register');
 });
 
+//register form POST
+router.post('/register',(req,res)=>{
+    let errors = [];
+
+    if(req.body.password != req.body.password2){
+        errors.push({
+            text:"you didnt type same password"
+        });
+    }
+
+    if(req.body.password < 4 && req.body.password2 < 4){
+        errors.push({
+            text:"password atleast 4 characters"
+        });
+    }
+
+    if(errors.length > 0 ){
+        res.render('users/register',{
+            errors:errors,
+            name:req.body.name,
+            email:req.body.email,
+            password:req.body.password,
+            password2:req.body.password2
+        });
+    } else {
+        res.send('passed');
+    }
+});
+
 module.exports = router;
